7770ded0e8388777d4a87986f1cf0e2727214a0b
Mercucio <laszlo.v21@gmail.com>
2025-12-22 16:35:29 +0100

    2nd iteration

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from stats import get_num_words, get_book_text
+from stats import get_num_words, get_book_text,get_num_characters,sort_character_counts
 
 def main():
     print(f"Found {get_num_words(get_book_text('books/frankenstein.txt'))} total words")
-
+    char_counts=get_num_characters(get_book_text('books/frankenstein.txt'))
+    sorted_char_counts=sort_character_counts(char_counts)
+    print(sorted_char_counts)
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -7,3 +7,17 @@ def get_book_text(filepath):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_characters(text):
+    char_counts= {}
+    for char in text.lower():
+        if char in char_counts:
+           counter = char_counts[char]
+           counter+= 1
+        else:
+           counter = 1
+        char_counts[char] = counter
+    return char_counts
+
+def sort_character_counts(char_counts):
+    return dict(sorted(char_counts.items(), key=lambda item: item[1], reverse=True))
